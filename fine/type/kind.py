ad377a7bba8c12cbf5f9e0af31f740acaf47a341
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-05-24 16:59:45 -0400

    simplify kind

--- fine/type/kind.py
+++ fine/type/kind.py
@@ -1,28 +1,24 @@
+from dataclasses import dataclass
+
+
 class Kind:
-    def __eq__(self, other):
-        return isinstance(other, type(self))
+    pass
 
 
+@dataclass
 class AtomKind(Kind):
     def __repr__(self):
-        return "Type"
+        return "*"
 
 
+@dataclass
 class FunctionKind(Kind):
-    def __init__(self, left: Kind, right: Kind):
-        self.left = left
-        self.right = right
+    left: Kind
+    right: Kind
 
     def __repr__(self):
         l = f"({self.left})" if isinstance(self.left, FunctionKind) else self.left
         return f"{l} -> {self.right}"
-
-    def __eq__(self, other):
-        return (
-            super().__eq__(other)
-            and self.left == other.left
-            and self.right == other.right
-        )
 
 
 ATOM = AtomKind()
