dd8d44b0467aa87de689d30c34626a96353d3dd1
Eduardo García Maleta <egmaleta.secondary@proton.me>
2024-06-08 09:16:02 -0400

    infer kind

--- fine/type.py
+++ fine/type.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from .utils import String
+from .utils import String, Env
 
 
 class Kind:
@@ -40,6 +40,16 @@ class FunctionKind(Kind):
             f"({kind})" if isinstance(kind, FunctionKind) else repr(kind)
             for kind in self.args
         )
+
+
+@dataclass
+class ConstraintKind(Kind):
+    kind: Kind
+
+    def __repr__(self):
+        return (
+            f"({self.kind})" if isinstance(self.kind, FunctionKind) else repr(self.kind)
+        ) + " -> Constraint"
 
 
 class Type:
@@ -100,13 +110,13 @@ class ConstrainedType(Type):
         self.type = type
 
         self.constraints = {}
-        for new_tclass, tvars in constraints:
-            for tvar in tvars:
-                tclasses = self.constraints.get(tvar)
-                if tclasses is not None:
-                    tclasses.add(new_tclass)
+        for new_tclass_name, tvar_names in constraints:
+            for name in tvar_names:
+                tclass_names = self.constraints.get(name)
+                if tclass_names is not None:
+                    tclass_names.add(new_tclass_name)
                 else:
-                    self.constraints[tvar] = {new_tclass}
+                    self.constraints[name] = {new_tclass_name}
 
     def __len__(self):
         return len(self.type)
@@ -167,3 +177,113 @@ class Quantifier:
         assert len(free) == 0
 
         return type
+
+
+type KindEnv = Env[Kind | None]
+
+
+class KindInferer:
+    def _assign(self, type: Type, env: KindEnv, kind: Kind):
+        match type:
+            case TypeVar(name) | TypeConstant(name):
+                assert env.set(name, kind)
+
+            case ConstrainedType(_, inner) | TypeScheme(_, inner):
+                self._assign(inner, env, kind)
+
+            case _:
+                assert False
+
+    def _infer(self, type: Type, env: KindEnv):
+        match type:
+            case TypeConstant(name) | TypeVar(name):
+                kind, found = env.get(name)
+                assert found
+
+                return kind
+
+            case TypeApp(f, args):
+                f_kind = self._infer(f, env)
+
+                if f_kind is not None:
+                    for type in args:
+                        assert isinstance(f_kind, FunctionKind)
+                        left_kind = f_kind.left
+
+                        kind = self._infer(type, env)
+                        if kind is not None:
+                            assert kind == left_kind
+                        else:
+                            self._assign(type, env, left_kind)
+
+                        f_kind = f_kind.right
+
+                    return f_kind
+
+                # 'kind(f)' = kind(arg1) -> kind(arg2) -> kind(arg3) -> ... -> *'
+                arg_kinds = []
+                for type in args:
+                    kind = self._infer(type, env)
+                    if kind is not None:
+                        arg_kinds.append(kind)
+                    else:
+                        self._assign(type, env, ATOM_KIND)
+                        arg_kinds.append(ATOM_KIND)
+
+                assert env.set(f.name, FunctionKind([*arg_kinds, ATOM_KIND]))
+
+                return ATOM_KIND
+
+            case FunctionType() as ftype:
+                # kind(->) = * -> * -> *
+
+                left_type = ftype.left
+                left_kind = self._infer(left_type, env)
+                if left_kind is not None:
+                    assert left_kind == ATOM_KIND
+                else:
+                    self._assign(left_type, env, ATOM_KIND)
+
+                right_type = ftype.right
+                right_kind = self._infer(right_type, env)
+                if right_kind is not None:
+                    assert right_kind == ATOM_KIND
+                else:
+                    self._assign(right_type, env, ATOM_KIND)
+
+                return ATOM_KIND
+
+            case ConstrainedType(constrs, inner):
+                # if typevar 'a' is instance of typeclass 'B' and 'kind(B)' is 'k -> Constraint'
+                # then 'kind(a)' is 'k'
+                for tvar_name, tclass_names in constrs.items():
+                    candidate = None
+                    for name in tclass_names:
+                        constr_kind, found = env.get(name)
+                        assert found
+                        assert isinstance(constr_kind, ConstraintKind)
+
+                        if candidate is not None:
+                            assert candidate == constr_kind.kind
+                        else:
+                            candidate = constr_kind.kind
+
+                    assert env.set(tvar_name, candidate)
+
+                return self._infer(inner, env)
+
+            case TypeScheme(vars, inner):
+                env = env.child_env()
+                for name in vars:
+                    env.add(name, None)
+
+                return self._infer(inner, env)
+
+    def infer(self, type: Type, env: KindEnv):
+        kind = self._infer(type, env)
+        if kind is not None:
+            assert kind == ATOM_KIND
+        else:
+            self._assign(type, env, ATOM_KIND)
+
+        return ATOM_KIND
